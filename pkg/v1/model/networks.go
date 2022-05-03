471b09627d9c42a5085a9ea5f02ca029d8aea971
Marc Dallen <marc@enviroservices.com.au>
2022-05-03 11:14:49 +1000

    changed polling time properties to int type.

--- pkg/v1/model/networks.go
+++ pkg/v1/model/networks.go
@@ -1,7 +1,5 @@
 package model
 
-import "time"
-
 type IPType struct {
 	REST string `json:"rest"`
 	UDP  string `json:"udp"`
@@ -31,28 +29,28 @@ type Network struct {
 	CommonThingClass
 	CommonThingRef
 	CommonThingType
-	TransportType                string        `json:"transport_type,omitempty"  gorm:"type:varchar(255);not null"` //serial
-	PluginConfId                 string        `json:"plugin_conf_id,omitempty" gorm:"TYPE:varchar(255) REFERENCES plugin_confs;not null;default:null"`
-	PluginPath                   string        `json:"plugin_name,omitempty"` //plugin_name
-	AutoMappingNetworksSelection string        `json:"auto_mapping_networks_selection"`
-	AutoMappingFlowNetworkUUID   string        `json:"auto_mapping_flow_network_uuid"`
-	AutoMappingFlowNetworkName   string        `json:"auto_mapping_flow_network_name"`
-	NumberOfNetworksPermitted    *int          `json:"number_of_networks_permitted,omitempty"`
-	NetworkInterface             string        `json:"network_interface"`
-	IP                           string        `json:"ip"`
-	Port                         *int          `json:"port"`
-	NetworkMask                  *int          `json:"network_mask"`
-	AddressID                    string        `json:"address_id"`
-	AddressUUID                  string        `json:"address_uuid"`
-	SerialPort                   *string       `json:"serial_port,omitempty" gorm:"type:varchar(255);unique"`
-	SerialBaudRate               *uint         `json:"serial_baud_rate,omitempty"` //9600
-	SerialStopBits               *uint         `json:"serial_stop_bits,omitempty"` //1 or 2
-	SerialParity                 *string       `json:"serial_parity,omitempty"`    //odd, even, none
-	SerialDataBits               *uint         `json:"serial_data_bits,omitempty"` //7 or 8
-	SerialTimeout                *int          `json:"serial_timeout,omitempty"`
-	SerialConnected              *bool         `json:"serial_connected,omitempty"`
-	Host                         *string       `json:"host,omitempty"`
-	Devices                      []*Device     `json:"devices,omitempty" gorm:"constraint:OnDelete:CASCADE"`
-	Tags                         []*Tag        `json:"tags,omitempty" gorm:"many2many:networks_tags;constraint:OnDelete:CASCADE"`
-	MaxPollRate                  time.Duration `json:"max_poll_rate,omitempty"`
+	TransportType                string    `json:"transport_type,omitempty"  gorm:"type:varchar(255);not null"` //serial
+	PluginConfId                 string    `json:"plugin_conf_id,omitempty" gorm:"TYPE:varchar(255) REFERENCES plugin_confs;not null;default:null"`
+	PluginPath                   string    `json:"plugin_name,omitempty"` //plugin_name
+	AutoMappingNetworksSelection string    `json:"auto_mapping_networks_selection"`
+	AutoMappingFlowNetworkUUID   string    `json:"auto_mapping_flow_network_uuid"`
+	AutoMappingFlowNetworkName   string    `json:"auto_mapping_flow_network_name"`
+	NumberOfNetworksPermitted    *int      `json:"number_of_networks_permitted,omitempty"`
+	NetworkInterface             string    `json:"network_interface"`
+	IP                           string    `json:"ip"`
+	Port                         *int      `json:"port"`
+	NetworkMask                  *int      `json:"network_mask"`
+	AddressID                    string    `json:"address_id"`
+	AddressUUID                  string    `json:"address_uuid"`
+	SerialPort                   *string   `json:"serial_port,omitempty" gorm:"type:varchar(255);unique"`
+	SerialBaudRate               *uint     `json:"serial_baud_rate,omitempty"` //9600
+	SerialStopBits               *uint     `json:"serial_stop_bits,omitempty"` //1 or 2
+	SerialParity                 *string   `json:"serial_parity,omitempty"`    //odd, even, none
+	SerialDataBits               *uint     `json:"serial_data_bits,omitempty"` //7 or 8
+	SerialTimeout                *int      `json:"serial_timeout,omitempty"`
+	SerialConnected              *bool     `json:"serial_connected,omitempty"`
+	Host                         *string   `json:"host,omitempty"`
+	Devices                      []*Device `json:"devices,omitempty" gorm:"constraint:OnDelete:CASCADE"`
+	Tags                         []*Tag    `json:"tags,omitempty" gorm:"many2many:networks_tags;constraint:OnDelete:CASCADE"`
+	MaxPollRate                  *int      `json:"max_poll_rate,omitempty"`
 }
