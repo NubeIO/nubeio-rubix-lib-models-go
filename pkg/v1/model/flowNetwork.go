5558c24ace25532ee2c3238fb2c5935b72061bf4
NubeDev <aidan@enviroservices.com.au>
2022-07-01 14:43:11 +1000

    flow network remove duplicate ip, port and streams

--- pkg/v1/model/flowNetwork.go
+++ pkg/v1/model/flowNetwork.go
@@ -5,28 +5,28 @@ type CommonFlowNetwork struct {
 	CommonSyncUUID
 	CommonName
 	CommonDescription
-	GlobalUUID              string    `json:"global_uuid,omitempty" gorm:"unique"`
-	ClientId                string    `json:"client_id,omitempty"`
-	ClientName              string    `json:"client_name,omitempty"`
-	SiteId                  string    `json:"site_id,omitempty"`
-	SiteName                string    `json:"site_name,omitempty"`
-	DeviceId                string    `json:"device_id,omitempty"`
-	DeviceName              string    `json:"device_name,omitempty"`
-	IsRemote                *bool     `json:"is_remote,omitempty"`
-	FetchHistories          *bool     `json:"fetch_histories,omitempty"`
-	FetchHistoriesFrequency int       `json:"fetch_hist_frequency,omitempty"`      //time example 15min
-	DeleteHistoriesOnFetch  *bool     `json:"delete_histories_on_fetch,omitempty"` //drop the histories on the producer device on migration
-	IsMasterSlave           *bool     `json:"is_master_slave,omitempty"`
-	FlowHTTPS               *bool     `json:"flow_https,omitempty"`
-	FlowIP                  *string   `json:"flow_ip,omitempty"`
-	FlowPort                *int      `json:"flow_port,omitempty"`
-	FlowUsername            *string   `json:"flow_username,omitempty"`
-	FlowPassword            *string   `json:"flow_password,omitempty"`
-	FlowToken               *string   `json:"flow_token,omitempty"`
-	IsTokenAuth             *bool     `json:"is_token_auth,omitempty"`
-	IsError                 *bool     `json:"is_error" gorm:"default:false"`
-	ErrorMsg                *string   `json:"error_msg,omitempty"`
-	Streams                 []*Stream `json:"streams" gorm:"many2many:flow_networks_streams;constraint:OnDelete:CASCADE"`
+	GlobalUUID              string `json:"global_uuid,omitempty" gorm:"unique"`
+	ClientId                string `json:"client_id,omitempty"`
+	ClientName              string `json:"client_name,omitempty"`
+	SiteId                  string `json:"site_id,omitempty"`
+	SiteName                string `json:"site_name,omitempty"`
+	DeviceId                string `json:"device_id,omitempty"`
+	DeviceName              string `json:"device_name,omitempty"`
+	IsRemote                *bool  `json:"is_remote,omitempty"`
+	FetchHistories          *bool  `json:"fetch_histories,omitempty"`
+	FetchHistoriesFrequency int    `json:"fetch_hist_frequency,omitempty"`      //time example 15min
+	DeleteHistoriesOnFetch  *bool  `json:"delete_histories_on_fetch,omitempty"` //drop the histories on the producer device on migration
+	IsMasterSlave           *bool  `json:"is_master_slave,omitempty"`
+	FlowHTTPS               *bool  `json:"flow_https,omitempty"`
+	//FlowIP                  *string   `json:"flow_ip,omitempty"`
+	//FlowPort                *int      `json:"flow_port,omitempty"`
+	FlowUsername *string `json:"flow_username,omitempty"`
+	FlowPassword *string `json:"flow_password,omitempty"`
+	FlowToken    *string `json:"flow_token,omitempty"`
+	IsTokenAuth  *bool   `json:"is_token_auth,omitempty"`
+	IsError      *bool   `json:"is_error" gorm:"default:false"`
+	ErrorMsg     *string `json:"error_msg,omitempty"`
+	//Streams                 []*Stream `json:"streams" gorm:"many2many:flow_networks_streams;constraint:OnDelete:CASCADE"`
 	CommonConnection
 	CommonCreated
 }
