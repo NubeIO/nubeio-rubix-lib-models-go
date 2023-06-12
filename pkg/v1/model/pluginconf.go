9d348a5f98d08970c8d5d335f0ca5c2a93d97e0e
RaiBnod <binodrayee@gmail.com>
2023-06-12 10:36:54 +0545

    Improvements on plugin model

--- pkg/v1/model/pluginconf.go
+++ pkg/v1/model/pluginconf.go
@@ -6,7 +6,6 @@ type PluginConf struct {
 	Name        string `json:"name" gorm:"name:varchar(255);not null"`
 	ModulePath  string `json:"module_path"  gorm:"type:varchar(255);unique;not null"`
 	Enabled     bool   `json:"enabled"`
-	HasNetwork  bool   `json:"has_network"`
 	Config      []byte
 	Storage     []byte
 	Network     Network     `json:"networks" gorm:"constraint:OnDelete:CASCADE"`
@@ -20,14 +19,13 @@ type PluginConfExternal struct {
 	UUID         string        `json:"uuid" sql:"uuid"  gorm:"type:varchar(255);unique;primaryKey"`
 	Name         string        `json:"name" gorm:"type:varchar(255);not null"`
 	ModulePath   string        `json:"module_path"  gorm:"type:varchar(255);unique;not null"`
-	Author       string        `json:"author,omitempty" form:"author" query:"author"`
-	Website      string        `json:"website,omitempty" form:"website" query:"website"`
-	License      string        `json:"license,omitempty" form:"license" query:"license"`
 	Enabled      bool          `json:"enabled"`
-	HasNetwork   bool          `json:"has_network"`
-	ProtocolType string        `json:"protocol_type" gorm:"type:varchar(255);not null"`
-	Capabilities []string      `json:"capabilities"`
 	Network      Network       `json:"networks" gorm:"constraint:OnDelete:CASCADE"`
 	Integration  []Integration `json:"integration" gorm:"constraint:OnDelete:CASCADE"`
 	Job          []Job         `json:"jobs" gorm:"constraint:OnDelete:CASCADE"`
+	Author       string        `json:"author,omitempty" form:"author" query:"author"`
+	Website      string        `json:"website,omitempty" form:"website" query:"website"`
+	License      string        `json:"license,omitempty" form:"license" query:"license"`
+	HasNetwork   bool          `json:"has_network"`
+	Capabilities []string      `json:"capabilities"`
 }
