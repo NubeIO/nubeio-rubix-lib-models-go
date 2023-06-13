59b63108366ed03ecbcb137ce01b003fd51b9844
RaiBnod <binodrayee@gmail.com>
2023-06-13 13:59:59 +0545

    Upgrade histories and history_postgres_logs

--- pkg/v1/model/history.go
+++ pkg/v1/model/history.go
@@ -4,9 +4,10 @@ import "time"
 
 // History for storing the all history
 type History struct {
-	HistoryID int       `json:"history_id" gorm:"primaryKey"`
-	ID        int       `json:"id" gorm:"uniqueIndex:idx_histories_id_uuid_value_timestamp"`
-	UUID      string    `json:"uuid" gorm:"uniqueIndex:idx_histories_id_uuid_value_timestamp"`
-	Value     float64   `json:"value" gorm:"uniqueIndex:idx_histories_id_uuid_value_timestamp"`
-	Timestamp time.Time `json:"timestamp" gorm:"uniqueIndex:idx_histories_id_uuid_value_timestamp"`
+	HistoryID int       `json:"history_id" gorm:"autoIncrement;primaryKey"`
+	ID        int       `json:"id" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
+	PointUUID string    `json:"point_uuid" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
+	HostUUID  string    `json:"host_uuid" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
+	Value     *float64  `json:"value" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
+	Timestamp time.Time `json:"timestamp" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
 }
--- pkg/v1/model/historyPostgresLog.go
+++ pkg/v1/model/historyPostgresLog.go
@@ -5,7 +5,8 @@ import "time"
 // HistoryPostgresLog for storing the all history postgres logs
 type HistoryPostgresLog struct {
 	ID        int       `json:"id" gorm:"primaryKey"`
-	UUID      string    `json:"uuid" gorm:"primaryKey"`
-	Value     float64   `json:"value" gorm:"primaryKey"`
-	Timestamp time.Time `json:"timestamp" gorm:"primaryKey"`
+	PointUUID string    `json:"point_uuid"`
+	HostUUID  string    `json:"host_uuid"`
+	Value     *float64  `json:"value"`
+	Timestamp time.Time `json:"timestamp"`
 }
