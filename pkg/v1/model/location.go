9dba31d6271e4ff277e267f2ccac30f7f7af3eec
enjuthulung <enjuthulung@gmail.com>
2023-06-15 14:33:04 +0545

    Add device info fields to location

--- pkg/v1/model/location.go
+++ pkg/v1/model/location.go
@@ -4,6 +4,14 @@ type Location struct {
 	CommonUUID
 	CommonNameUnique
 	Description string   `json:"description"`
+	Address     string   `json:"address"`
+	City        string   `json:"city"`
+	State       string   `json:"state"`
+	Zip         string   `json:"zip"`
+	Country     string   `json:"country"`
+	Lat         string   `json:"lat"`
+	Lon         string   `json:"lon"`
+	TimeZone    string   `json:"time_zone"`
 	Groups      []*Group `json:"groups" gorm:"constraint:OnDelete:CASCADE"`
 	Views       []*View  `json:"views" gorm:"constraint:OnDelete:CASCADE"`
 }
