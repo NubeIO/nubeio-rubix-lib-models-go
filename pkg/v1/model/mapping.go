b0a074895b44eeb154b253b8b79b6c7c32315c86
enjuthulung <enjuthulung@gmail.com>
2023-06-26 13:46:51 +0545

    Remove flow-network related models

--- pkg/v1/model/mapping.go
+++
@@ -1,9 +0,0 @@
-package model
-
-type PointMapping struct {
-	AutoMappingNetworksSelection []string `json:"auto_mapping_networks_selection"` // user selected networks
-	AutoMappingFlowNetworkName   string   `json:"auto_mapping_flow_network_name"`  // user can send name
-	AutoMappingFlowNetworkUUID   string   `json:"auto_mapping_flow_network_uuid"`
-	SourceNetworkUUID            string   `json:"source_network_uuid"`
-	Point                        *Point   `json:"point"`
-}
--- pkg/v1/model/tags.go
+++ pkg/v1/model/tags.go
@@ -1,12 +1,8 @@
 package model
 
 type Tag struct {
-	Tag          string         `json:"tag" gorm:"type:varchar(255);unique;not null;default:null;primaryKey"`
-	Networks     []*Network     `json:"networks,omitempty" gorm:"many2many:networks_tags;constraint:OnDelete:CASCADE"`
-	Devices      []*Device      `json:"devices,omitempty" gorm:"many2many:devices_tags;constraint:OnDelete:CASCADE"`
-	Points       []*Point       `json:"points,omitempty" gorm:"many2many:points_tags;constraint:OnDelete:CASCADE"`
-	Streams      []*Stream      `json:"streams,omitempty" gorm:"many2many:streams_tags;constraint:OnDelete:CASCADE"`
-	StreamClones []*StreamClone `json:"stream_clones,omitempty" gorm:"many2many:stream_clones_tags;constraint:OnDelete:CASCADE"`
-	Producers    []*Producer    `json:"producers,omitempty" gorm:"many2many:producers_tags;constraint:OnDelete:CASCADE"`
-	Consumers    []*Consumer    `json:"consumers,omitempty" gorm:"many2many:consumers_tags;constraint:OnDelete:CASCADE"`
+	Tag      string     `json:"tag" gorm:"type:varchar(255);unique;not null;default:null;primaryKey"`
+	Networks []*Network `json:"networks,omitempty" gorm:"many2many:networks_tags;constraint:OnDelete:CASCADE"`
+	Devices  []*Device  `json:"devices,omitempty" gorm:"many2many:devices_tags;constraint:OnDelete:CASCADE"`
+	Points   []*Point   `json:"points,omitempty" gorm:"many2many:points_tags;constraint:OnDelete:CASCADE"`
 }
