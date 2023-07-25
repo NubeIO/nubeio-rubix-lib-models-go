2c574dd99f33c1f9ccc5c8793a1880bcb7173c80
enjuthulung <enjuthulung@gmail.com>
2023-07-25 13:54:46 +0545

    Add history_enable field in host

--- pkg/v1/model/host.go
+++ pkg/v1/model/host.go
@@ -3,27 +3,28 @@ package model
 import "time"
 
 type Host struct {
-	UUID           string         `json:"uuid" gorm:"primaryKey"`
-	GlobalUUID     string         `json:"global_uuid"`
-	DeviceType     string         `json:"device_type"`
-	Name           string         `json:"name"  gorm:"type:varchar(255);not null;uniqueIndex:idx_hosts_name_group_uuid"`
-	GroupUUID      string         `json:"group_uuid,omitempty" gorm:"TYPE:varchar(255) REFERENCES groups;not null;default:null;uniqueIndex:idx_hosts_name_group_uuid"`
-	Enable         *bool          `json:"enable"`
-	Description    string         `json:"description"`
-	IP             string         `json:"ip"`
-	BiosPort       int            `json:"bios_port"`
-	Port           int            `json:"port"`
-	HTTPS          *bool          `json:"https"`
-	IsOnline       *bool          `json:"is_online"`
-	IsValidToken   *bool          `json:"is_valid_token"`
-	ExternalToken  string         `json:"external_token"`
-	VirtualIP      string         `json:"virtual_ip"`
-	ReceivedBytes  int            `json:"received_bytes"`
-	SentBytes      int            `json:"sent_bytes"`
-	ConnectedSince string         `json:"connected_since"`
-	Tags           []*HostTag     `json:"tags,omitempty" gorm:"constraint:OnDelete:CASCADE"`
-	Comments       []*HostComment `json:"comments,omitempty" gorm:"constraint:OnDelete:CASCADE"`
-	Views          []*View        `json:"views" gorm:"constraint:OnDelete:CASCADE"`
+	UUID           string `json:"uuid" gorm:"primaryKey"`
+	GlobalUUID     string `json:"global_uuid"`
+	DeviceType     string `json:"device_type"`
+	Name           string `json:"name"  gorm:"type:varchar(255);not null;uniqueIndex:idx_hosts_name_group_uuid"`
+	GroupUUID      string `json:"group_uuid,omitempty" gorm:"TYPE:varchar(255) REFERENCES groups;not null;default:null;uniqueIndex:idx_hosts_name_group_uuid"`
+	Enable         *bool  `json:"enable"`
+	Description    string `json:"description"`
+	IP             string `json:"ip"`
+	BiosPort       int    `json:"bios_port"`
+	Port           int    `json:"port"`
+	HTTPS          *bool  `json:"https"`
+	IsOnline       *bool  `json:"is_online"`
+	IsValidToken   *bool  `json:"is_valid_token"`
+	ExternalToken  string `json:"external_token"`
+	VirtualIP      string `json:"virtual_ip"`
+	ReceivedBytes  int    `json:"received_bytes"`
+	SentBytes      int    `json:"sent_bytes"`
+	ConnectedSince string `json:"connected_since"`
+	CommonHistoryEnable
+	Tags     []*HostTag     `json:"tags,omitempty" gorm:"constraint:OnDelete:CASCADE"`
+	Comments []*HostComment `json:"comments,omitempty" gorm:"constraint:OnDelete:CASCADE"`
+	Views    []*View        `json:"views" gorm:"constraint:OnDelete:CASCADE"`
 }
 
 type HostTag struct {
