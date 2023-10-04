5932ec566fd0ee3733c9a8d387b69c8401870eb8
enjuthulung <enjuthulung@gmail.com>
2023-10-04 15:19:29 +0545

    Remove value as uniqueIndex from history

--- pkg/v1/model/history.go
+++ pkg/v1/model/history.go
@@ -5,9 +5,9 @@ import "time"
 // History for storing the all history
 type History struct {
 	HistoryID int       `json:"history_id" gorm:"autoIncrement;primaryKey"`
-	ID        int       `json:"id" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
-	PointUUID string    `json:"point_uuid" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
-	HostUUID  string    `json:"host_uuid" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
-	Value     *float64  `json:"value" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
-	Timestamp time.Time `json:"timestamp" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_value_timestamp"`
+	ID        int       `json:"id" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_timestamp"`
+	PointUUID string    `json:"point_uuid" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_timestamp"`
+	HostUUID  string    `json:"host_uuid" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_timestamp"`
+	Value     *float64  `json:"value"`
+	Timestamp time.Time `json:"timestamp" gorm:"uniqueIndex:idx_histories_id_point_uuid_host_uuid_timestamp"`
 }
