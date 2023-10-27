0ee7628566996069eae4669c73932fcc94bad2af
RaiBnod <binodrayee@gmail.com>
2023-10-27 15:19:08 +0545

    Fix: EmailConfig enable bool type

--- pkg/v1/model/emailconfig.go
+++ pkg/v1/model/emailconfig.go
@@ -12,7 +12,7 @@ const (
 type EmailConfig struct {
 	CommonUUID
 	Type      EmailConfigType `json:"type" gorm:"default:SMTP"`
-	Enable    bool            `json:"enable" gorm:"default:true"`
+	Enable    *bool           `json:"enable" gorm:"default:true"`
 	QueueSize int             `json:"queue_size" gorm:"default:10"`
 	Config    datatypes.JSON  `json:"config"`
 }
