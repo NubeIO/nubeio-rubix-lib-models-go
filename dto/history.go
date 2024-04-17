cf2dc892a6f3aac2ca5cbf1115d738de09d45aaf
enjuthulung <enjuthulung@gmail.com>
2024-04-17 15:09:12 +0545

    Add network, device and point tags and meta tags in history data

--- dto/history.go
+++ dto/history.go
@@ -11,49 +11,37 @@ type HistoryRequest struct {
 }
 
 type HistoryResponse struct {
-	Data     []*HistoryData   `json:"data"`
-	Tags     *HistoryTags     `json:"tags,omitempty"`
-	MetaTags *HistoryMetaTags `json:"meta_tags,omitempty"`
+	Data []*HistoryData `json:"data"`
 }
 
 type HistoryData struct {
-	LocationUUID        string          `json:"location_uuid"`
-	LocationName        string          `json:"location_name"`
-	LocationDescription string          `json:"location_description"`
-	GroupUUID           string          `json:"group_uuid"`
-	GroupName           string          `json:"group_name"`
-	GroupDescription    string          `json:"group_description"`
-	HostUUID            string          `json:"host_uuid"`
-	HostName            string          `json:"host_name"`
-	HostDescription     string          `json:"host_description"`
-	GlobalUUID          string          `json:"global_uuid"`
-	NetworkUUID         string          `json:"network_uuid"`
-	NetworkName         string          `json:"network_name"`
-	NetworkDescription  string          `json:"network_description"`
-	DeviceUUID          string          `json:"device_uuid"`
-	DeviceName          string          `json:"device_name"`
-	DeviceDescription   string          `json:"device_description"`
-	PointUUID           string          `json:"point_uuid"`
-	PointName           string          `json:"point_name"`
-	PointDescription    string          `json:"point_description"`
-	Values              []*HistoryValue `json:"values"`
+	LocationUUID        string            `json:"location_uuid"`
+	LocationName        string            `json:"location_name"`
+	LocationDescription string            `json:"location_description"`
+	GroupUUID           string            `json:"group_uuid"`
+	GroupName           string            `json:"group_name"`
+	GroupDescription    string            `json:"group_description"`
+	HostUUID            string            `json:"host_uuid"`
+	HostName            string            `json:"host_name"`
+	HostDescription     string            `json:"host_description"`
+	GlobalUUID          string            `json:"global_uuid"`
+	NetworkUUID         string            `json:"network_uuid"`
+	NetworkName         string            `json:"network_name"`
+	NetworkDescription  string            `json:"network_description"`
+	DeviceUUID          string            `json:"device_uuid"`
+	DeviceName          string            `json:"device_name"`
+	DeviceDescription   string            `json:"device_description"`
+	PointUUID           string            `json:"point_uuid"`
+	PointName           string            `json:"point_name"`
+	PointDescription    string            `json:"point_description"`
+	Values              []*HistoryValue   `json:"values"`
+	Tags                []*HistoryTag     `json:"tags,omitempty"`
+	MetaTags            []*HistoryMetaTag `json:"meta_tags,omitempty"`
 }
 
 type HistoryValue struct {
 	Value     float64   `json:"value"`
 	Timestamp time.Time `json:"timestamp"`
-}
-
-type HistoryTags struct {
-	Network map[string][]*HistoryTag `json:"network,omitempty"`
-	Device  map[string][]*HistoryTag `json:"device,omitempty"`
-	Point   map[string][]*HistoryTag `json:"point,omitempty"`
-}
-
-type HistoryMetaTags struct {
-	Network map[string][]*HistoryMetaTag `json:"network,omitempty"`
-	Device  map[string][]*HistoryMetaTag `json:"device,omitempty"`
-	Point   map[string][]*HistoryMetaTag `json:"point,omitempty"`
 }
 
 type HistoryTag struct {
