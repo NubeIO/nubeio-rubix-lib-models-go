41f9db31b523228eb1c03e57a756f2910cac3a83
Enju Thulung <enjuthulung@gmail.com>
2024-09-09 08:57:29 +0545

    Remove expression IFNULL from idx_networks_name_host_uuid index (#143)

--- model/pointhistory.go
+++ model/pointhistory.go
@@ -8,5 +8,5 @@ type PointHistory struct {
 	ID        int       `json:"id" gorm:"autoIncrement;primaryKey;index"`
 	PointUUID string    `json:"point_uuid,omitempty" gorm:"type:varchar(255);not null;default:null;uniqueIndex:idx_point_histories_point_uuid_timestamp"`
 	Value     *float64  `json:"present_value"`
-	Timestamp time.Time `json:"timestamp,omitempty" gorm:"uniqueIndex:idx_point_histories_point_uuid_timestamp,expression:IFNULL(timestamp\\,\"\")"`
+	Timestamp time.Time `json:"timestamp,omitempty" gorm:"uniqueIndex:idx_point_histories_point_uuid_timestamp"`
 }
