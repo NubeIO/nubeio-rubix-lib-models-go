d08dc9dcdf2c7e5b7f9d5ccfe2f85457ba92b411
Enju Thulung <enjuthulung@gmail.com>
2026-02-27 14:22:01 +0545

    Add AutoRecoveryCount (#156)

--- model/plugin.go
+++ model/plugin.go
@@ -2,12 +2,13 @@ package model
 
 // Plugin holds information about the plugin.
 type Plugin struct {
-	UUID         string `json:"uuid" sql:"uuid"  gorm:"type:varchar(255);unique;primaryKey"`
-	Name         string `json:"name"  gorm:"type:varchar(255);unique;not null"`
-	Enabled      bool   `json:"enabled"`
-	MessageLevel string `json:"message_level"`
-	Message      string `json:"message"`
-	Config       []byte
-	Storage      []byte
-	Network      Network `json:"networks" gorm:"constraint:OnDelete:CASCADE"`
+	UUID              string `json:"uuid" sql:"uuid"  gorm:"type:varchar(255);unique;primaryKey"`
+	Name              string `json:"name"  gorm:"type:varchar(255);unique;not null"`
+	Enabled           bool   `json:"enabled"`
+	MessageLevel      string `json:"message_level"`
+	Message           string `json:"message"`
+	AutoRecoveryCount int    `json:"auto_recovery_count"`
+	Config            []byte
+	Storage           []byte
+	Network           Network `json:"networks" gorm:"constraint:OnDelete:CASCADE"`
 }
